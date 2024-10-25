fb643c931ff79f3fd3b2dc96b4bb17767103c3ec
Hitarth Chudgar <hitarthchudgar@gmail.com>
2024-10-25 15:51:14 -0400

    updated font weight

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         alt="MotionCraft Logo"
       />
       <div className="flex flex-col items-end justify-end gap-3">
-        <h1 className="text-right font-pt-serif font-normal text-zinc-50 leading-tight w-full text-3xl sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+        <h1 className="text-right font-pt-serif font-bold text-zinc-50 leading-tight w-full text-3xl sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
           Collection of UI experiments
         </h1>
         <p className="text-right font-pt-serif font-normal text-zinc-50 leading-tight text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl">
