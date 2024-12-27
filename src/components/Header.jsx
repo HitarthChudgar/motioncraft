a8beb775e98b774758cc3268f63894d15a9a3f83
Hitarth Chudgar <hitarthchudgar@gmail.com>
2024-12-26 23:19:47 -0500

    added glitch effect and url redirect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import logo from "../assets/motioncraft_logo.png";
+import { useGlitch } from "react-powerglitch";
 
 const Header = () => {
+  const glitch = useGlitch();
   return (
     <div className="flex justify-between items-top w-full h-auto gap-6">
       <img
@@ -14,9 +16,15 @@ const Header = () => {
         </h1>
         <p className="text-right font-pt-serif font-normal text-zinc-50 leading-tight text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl">
           Designed and built by{" "}
-          <span className="font-bold underline bg-clip-text text-transparent bg-gradient-to-r from-purple-300 via-pink-200 to-purple-300 bg-[length:200%_100%] animate-shimmer">
+          <a
+            href="https://www.linkedin.com/in/hitarthsc/"
+            target="_blank"
+            rel="noopener noreferrer"
+            ref={glitch.ref}
+            className="font-bold underline bg-clip-text text-transparent hover:bg-purple-300 bg-gradient-to-r from-purple-300 via-pink-200 to-purple-300 bg-[length:200%_100%] animate-shimmer"
+          >
             Hitarth
-          </span>
+          </a>
         </p>
       </div>
     </div>
